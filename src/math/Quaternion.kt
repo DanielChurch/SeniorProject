0c96a78bc06019697efb8a7cf1b114511c865af2
Dieter103 <grosswiler2@gmail.com>
2018-02-06 10:45:13 -0700

    a

--- src/math/Quaternion.kt
+++ src/math/Quaternion.kt
@@ -1,7 +1,30 @@
 package math
 
-class Quaternion() {
-    companion object {
-        val zero = Quaternion()
+import kotlin.js.Math
+import kotlin.js.Math.abs
+
+open class Quaternion(var w:Float, var x: Float, var y: Float, var z: Float) {
+    private val TOLERANCE = 0.00001f
+//    companion object {
+//        val zero = Quaternion()
+//    }
+    //Normalizes Quaternion unless it is within set tolerance
+    fun normalization(quaternion: Quaternion) {
+        var q = Quaternion(0f,0f,0f,0f)
+        var mag2 = quaternion.w * quaternion.w + quaternion.x * quaternion.x + quaternion.y * quaternion.y + quaternion.z * quaternion.z
+        if (abs(((mag2 - 1.0f).toDouble())) > TOLERANCE) {
+            var mag = Math.sqrt(mag2.toDouble()).toFloat()
+            w /= mag
+            x /= mag
+            y /= mag
+            z /= mag
+        }
     }
+
+    fun getConjugate(quaternion: Quaternion): Quaternion {
+        return Quaternion(-x, -y, -z, w)
+    }
+
+
+
 }
